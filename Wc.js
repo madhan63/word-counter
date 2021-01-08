f8745997692798fbc35164692d333e1e4367286a
Madhan06 <63894524+madhan63@users.noreply.github.com>
2021-01-08 13:10:19 +0530

    Add files via upload

--- Wc.js
+++ Wc.js
@@ -1,15 +1,15 @@
 function count()
 {
-var noofwords=document.getElementById("WordCount").value;
-noofwords=noofwords.match(/\w+/g);
-if(noofwords.length<201)
-{
-document.getElementById("count").innerHTML="Number of words written: "+noofwords.length;
-var r=200-noofwords.length;
-document.getElementById("remaining").innerHTML="Number of words remaining: "+r;
-}
-else
-{
-alert("You have exced the words limit,please enter the words less than or equal to 200.\n\n Number of words entered: "+noofwords.length);
-}
+    var noofwords=document.getElementById("WordCount").value;
+    noofwords=noofwords.match(/\w+/g);
+    if(noofwords.length<201)
+    {
+        document.getElementById("count").innerHTML="Number of words written: "+noofwords;
+        var r=200-noofwords.length;
+        document.getElementById("remaining").innerHTML="Number of words remaining: "+r;
+    }
+    else
+    {
+        alert("You have exced the words limit,please enter the words less than or equal to 200.\n\n Number of words entered: "+noofwords.length);
+    }
 }
